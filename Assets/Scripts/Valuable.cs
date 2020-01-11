44506977f8d0aceec88d516c28d8225148961622
Finnbarr O'Callahan <lukasocallahan@gmail.com>
2020-01-11 14:17:48 -0500

    Merged cone logic and magnetism code

--- Assets/Scripts/Valuable.cs
+++ Assets/Scripts/Valuable.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class Valuable : MonoBehaviour
 {
     public int price;
+    public GameObject player;
+    public float magnetism_strength = 0.2f;
+
+    private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float closeness = Mathf.Max(0, 10 - Vector3.Distance(transform.position, target.position));
+
+        if (closeness > 0)
+        {
+            closeness = Mathf.Pow(closeness, 2);
+            float step = magnetism_strength * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step * closeness);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GameObject.ReferenceEquals(collision.gameObject, player))
+        {
+            // TODO: call scoreboard function, passing this.gameObject
+            Debug.Log("Passing this.gameObject to scoreboard");
+            Destroy(this.gameObject);
+        }
     }
 }
