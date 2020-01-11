6a90eaed935396cbb56e3da78c2e7b35adb7a520
Kyurem <haohua.tang@mail.utoronto.ca>
2020-01-11 13:27:01 -0500

    Scoreboard should work. Need Metal class and its type attribute though.

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //public for debuging
+    public int score;
+    Dictionary<string, int> scoreDict;
+    Text text;
+    void Start()
+    {
+        scoreDict = new Dictionary<string, int>
+        {
+            {"Ring", 100},
+            {"Necklace",200},
+            {"Scrap",5}
+        };
+        score = 0;
+        text = GetComponent<Text>();
+    }
+
+    public void handleCollision(Metal metal)
+    {
+        string metalType = metal.type();
+        score += scoreDict[metalType];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = score.ToString("0.");
+    }
+}
