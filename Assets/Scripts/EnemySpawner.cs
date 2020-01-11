c78313dae997eacc8b9b0a9be4a6411e9bbe9b43
Alex <jurcaua@gmail.com>
2020-01-11 14:44:18 -0500

    Enemy logic and spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform player;
+    public GameObject enemyPrefab;
+    public float spawnRate;
+    public float enemySpeed;
+
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime > spawnRate)
+        {
+            GameObject createdEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            EnemyFollow enemyFollow = createdEnemy.GetComponent<EnemyFollow>();
+            if (enemyFollow)
+            {
+                enemyFollow.followTarget = player;
+                enemyFollow.speed = enemySpeed;
+            }
+
+            // Reset the time
+            startTime = Time.time;
+        }
+    }
+}
